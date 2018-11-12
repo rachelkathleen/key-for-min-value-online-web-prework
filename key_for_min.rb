f204a1c1870dc712313a047fd7ff53a33ca7bc52
Rachel Hawa <rachel.hawa@gmail.com>
2018-11-12 01:10:22 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
   low_key = nil
   low_value = nil
   name_hash.collect do |key, value|
-    if low_value == nil || v < low_value
+    if low_value == nil || value < low_value
       low_value = value
       low_key = key
     end
